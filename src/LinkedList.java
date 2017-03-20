4468884c5f85a96a5e88846be111925725ddec5d
Taxxon <te15.emil.kack@umea.nti.se>
2017-03-20 09:10:57 +0100

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Emil Käck on 2017-03-20.
+ */
+public class LinkedList {
+
+    node firs;
+    node last;
+
+    private class node{
+        String data;
+        node next;
+    }
+}
